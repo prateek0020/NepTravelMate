dfd1f4a75ab431c6447c105067256099f6dd2b54
prateek0020 <prateek0020@gmail.com>
2014-05-31 12:22:46 +0545

    NepTravelMate
    Tourist Travel Guide App

---
+++ NepTravelMate/src/com/pathfinder/neptravelmate/GoogleMapView.java
@@ -0,0 +1,17 @@
+package com.pathfinder.neptravelmate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoogleMapView extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.google_map_view);
+	}
+	
+	
+
+}
